9ab7a8baa1c5ba4524f246a181e75b68c9b15840
milindsingh98 <31711593+milindsingh98@users.noreply.github.com>
2019-05-26 18:50:05 +0530

    wordtomorse.C
    C code for converting the sentence to morse code

---
+++ wordtomorse.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+  int len,i;
+  char sen[100];
+  printf("Enter a sentence\n");
+  scanf("%s",sen);
+  len = stelen(sen);
+  
